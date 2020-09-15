92230976a795a4922064d79960be3d71a6ae03c4
CBJ_MD <cbjmd15511661@gmail.com>
2020-09-15 19:33:21 +0900

    Added36 & 69

---
+++ C#/LeetCodePractice/Problems/36.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+
+namespace LeetCodePractice.Problems.Problem36
+{
+    public class Solution
+    {
+        const int _BLANK = 46;
+
+        public bool IsValidSudoku(char[][] board)
+        {
+            return Method1(board);
+        }
+
+        public char[,] SolveSudoku(char[,] board)
+        {
+            bool result = false;
+            Method1_DFS(0, board, ref result);
+            return board;
+        }
+
+        #region Method1 DFS(Naive)
+        private bool Method1(char[][] board)
+        {
+            bool result = false;
+            //Method1_DFS(0, board, ref result);
+            return result;
+        }
+
+        private static int total = 0;
+
+        private void Method1_DFS(int depth, char[,] board, ref bool result)
+        {
+            if (depth == board.Length)
+            {
+                result = true;
+                return;
+            }
+
+            int column = depth % 9;
+            int row = depth / 9;
+
+            Console.WriteLine($"processing: total: {total++}, depth:{depth}, column: {column}, row: {row}");
+
+            if (board[column, row] == _BLANK)
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    if (Method1_IsValid(column, row, i, board))
+                    {
+                        board[column, row] = (char)i;
+                        Method1_DFS(depth + 1, board, ref result);
+                        board[column, row] = (char)_BLANK;
+                    }
+                }
+                return;
+            }
+            else
+            {
+                Method1_DFS(depth + 1, board, ref result);
+            }
+        }
+
+        private bool Method1_IsValid(int column, int row, int num, char[,] board)
+        {
+            int cellColumn = column / 3;
+            int cellRow = row / 3;
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[column, i] == num ||
+                    board[i, row] == num ||
+                    board[i % 3 + cellColumn * 3, i / 3 + cellRow * 3] == num)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ C#/LeetCodePractice/Problems/69.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace LeetCodePractice.Problems.Problem69
+{
+    public class Solution
+    {
+        public int MySqrt(int x)
+        {
+            return Method1(x);
+        }
+
+        /// <summary>
+        /// 二分法
+        /// </summary>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private int Method1(int y)
+        {
+            if (y == 0 || y == 1)
+            {
+                return y;
+            }
+
+            int l = 0;
+            int u = y;
+            while (l <= u)
+            {
+                int mid = (l + u) / 2;
+                if (y / mid == mid)
+                {
+                    return mid;
+                }
+
+                if (y / mid < mid) // mid * mid会导致int溢出
+                {
+                    u = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            return u;
+        }
+
+        /// <summary>
+        /// 牛顿迭代
+        /// </summary>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private int Method2(int y)
+        {
+            if (y == 0 || y == 1)
+            {
+                return y;
+            }
+
+            double x = y; // 单精度无法通过2147395599
+            while (x > y / x)
+            {
+                x = x / 2 + y / (2 * x);
+                x = (int)x; // 向下圆整
+            }
+            return (int)x;
+        }
+    }
+}
