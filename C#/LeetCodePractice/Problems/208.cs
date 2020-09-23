8ec1487ac8712ed0cb7d0999d6f73010ba568566
CBJ_MD <cbjmd15511661@gmail.com>
2020-09-23 21:27:29 +0900

    208 Array approach

--- C#/LeetCodePractice/Problems/208.cs
+++ C#/LeetCodePractice/Problems/208.cs
@@ -6,6 +6,8 @@ using System.Text;
 
 namespace LeetCodePractice.Problems.Problem208
 {
+    #region Dummy
+#if false
     internal class TrieNode
     {
         public TrieNode() {; }
@@ -34,11 +36,11 @@ namespace LeetCodePractice.Problems.Problem208
 
     public class Trie
     {
-        #region fields
+    #region fields
         Dictionary<int, TrieNode> _nodes = new Dictionary<int, TrieNode>();
-        #endregion
+    #endregion
 
-        #region Interface
+    #region Interface
         /** Initialize your data structure here. */
         public Trie()
         {
@@ -149,9 +151,86 @@ namespace LeetCodePractice.Problems.Problem208
             }
 
         }
-        #endregion
+    #endregion
+    }
+#endif
+    #endregion
+
+    #region Array approach
+    internal class TrieNode
+    {
+        public TrieNode(char val) { Value = val; }
+
+        public char Value { get; private set; }
+
+        public bool IsEnd { get; set; }
+
+        public TrieNode[] ChildNodes = new TrieNode[26];
     }
 
+    public class Trie
+    {
+        #region fields
+        TrieNode _rootNode = new TrieNode('\n');
+        #endregion
+
+        #region Interface
+        /** Initialize your data structure here. */
+        public Trie()
+        {
+        }
+
+        /** Inserts a word into the trie. */
+        public void Insert(string word)
+        {
+            TrieNode current = _rootNode;
+            for (int i = 0; i < word.Length; i++)
+            {
+                int index = word[i] - 'a';
+                if (current.ChildNodes[index] == null)
+                {
+                    TrieNode node = new TrieNode(word[i]);
+                    current.ChildNodes[index] = node;
+                }
+                current = current.ChildNodes[index];
+            }
+            current.IsEnd = true;
+        }
+
+        /** Returns if the word is in the trie. */
+        public bool Search(string word)
+        {
+            TrieNode current = _rootNode;
+            for (int i = 0; i < word.Length; i++)
+            {
+                int index = word[i] - 'a';
+                if (current.ChildNodes[index] == null)
+                {
+                    return false;
+                }
+                current = current.ChildNodes[index];
+            }
+            return current.IsEnd;
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public bool StartsWith(string prefix)
+        {
+            TrieNode current = _rootNode;
+            for (int i = 0; i < prefix.Length; i++)
+            {
+                int index = prefix[i] - 'a';
+                if (current.ChildNodes[index] == null)
+                {
+                    return false;
+                }
+                current = current.ChildNodes[index];
+            }
+            return current.IsEnd || current.ChildNodes.Any(item => item != null);
+        }
+        #endregion
+    }
+    #endregion
     /**
      * Your Trie object will be instantiated and called as such:
      * Trie obj = new Trie();
@@ -159,4 +238,5 @@ namespace LeetCodePractice.Problems.Problem208
      * bool param_2 = obj.Search(word);
      * bool param_3 = obj.StartsWith(prefix);
      */
+
 }
