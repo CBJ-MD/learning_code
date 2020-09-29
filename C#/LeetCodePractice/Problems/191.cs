dc7ca201511340732098f718d2dc06e8743f3d30
CBJ_MD <cbjmd15511661@gmail.com>
2020-09-29 15:08:11 +0900

    added191

---
+++ C#/LeetCodePractice/Problems/191.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems.Problem191
+{
+    public class Solution
+    {
+        public int HammingWeight(uint n)
+        {
+            return AjustedMethod(n);
+        }
+
+        private int AjustedMethod(uint n)
+        {
+            uint count = 0;
+            for (; n > 0; n &= (n-1))
+            {
+                count++;
+            }
+            return (int)count;
+        }
+
+        private int DummyMethod(uint n)
+        {
+            uint count = 0;
+            while (n != 0 )
+            {
+                count += n %2;
+                n >>= 1;
+            }
+            return (int)count;
+        }
+
+        
+    }
+}
