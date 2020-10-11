b6069ad8752759a0fb40833d03861df59f2f8b57
CBJ_MD <cbjmd15511661@gmail.com>
2020-10-11 19:55:04 +0900

    Added 231 & 338

---
+++ C#/LeetCodePractice/Problems/231.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems.Problem231
+{
+    public class Solution
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            return n > 0 && (n & (n - 1)) == 0;
+        }
+    }
+}
---
+++ C#/LeetCodePractice/Problems/338.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice.Problems.Problem338
+{
+    public class Solution
+    {
+        public int[] CountBits(int num)
+        {
+            return Dummy(num);
+        }
+
+        /// <summary>
+        /// 204 ms	29.6 MB
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private int[] Dummy(int num)
+        {
+            int[] res = new int[num + 1];
+            for (int i = 0; i < res.Length; i++)
+            {
+                int count = 0;
+                int current = i;
+                while (current != 0)
+                {
+                    current &= current - 1;
+                    count++;
+                }
+                res[i] = count;
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 212 ms	29.8 MB
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private int[] Advanced(int num)
+        {
+            int[] res = new int[num + 1];
+            for (int i = 1; i < res.Length; i++)
+            {
+                res[i] = res[i & (i - 1)] + 1;
+            }
+            return res;
+        }
+    }
+}
