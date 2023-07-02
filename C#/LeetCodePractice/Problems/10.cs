1bd591ac21080468e21ca4d6e3b19f88431b9caa
CBJ_MD <cbjmd15511661@gmail.com>
2023-07-02 19:32:17 +0800

    Completed  problem 10. Regular Expression Matching

--- C#/LeetCodePractice/Problems/10.cs
+++ C#/LeetCodePractice/Problems/10.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCodePractice.Problems.Problem10
 {
@@ -14,38 +11,62 @@ namespace LeetCodePractice.Problems.Problem10
             int lenP = p.Length;
             bool[,] res = new bool[lenP + 1, lenS + 1];
             res[0, 0] = true;
+
+            for (int i = 0; i < lenP; i++)
+            {
+                if (p[i] == '*')
+                {
+                    res[i + 1, 0] = res[i - 1, 0];
+                }
+            }
+
             for (int i = 1; i <= lenP; i++)
             {
                 for (int j = 1; j <= lenS; j++)
                 {
                     if (p[i - 1] == '*')
                     {
-                        if (IsMatch(s, p, i - 1, j))
+                        if (IsMatch(s, p, i - 2, j - 1))
                         {
-                            res[i, j] = res[i, j - 1] || res[i - 1, j - 1];
+                            res[i, j] = res[i, j - 1] || res[i - 1, j - 1] || res[i - 2, j];
                         }
                         else
                         {
-                            res[i, j] = res[i - 2, j - 1];
+                            res[i, j] = res[i - 2, j];
                         }
                     }
                     else
                     {
-                        res[i, j] = res[i - 1, j - 1] & IsMatch(s, p, i, j);
+                        res[i, j] = res[i - 1, j - 1] && IsMatch(s, p, i - 1, j - 1);
                     }
                 }
             }
-            Console.WriteLine(res[lenP, lenS]);
             return res[lenP, lenS];
         }
 
         private static bool IsMatch(string s, string p, int indexP, int indexS)
         {
-            indexS--;
-            indexP--;
             if (indexP < 0) return false;
             if (p[indexP] == '.') return true;
             return s[indexS] == p[indexP];
+        }
+
+        /// <summary>
+        /// for debugging
+        /// </summary>
+        /// <param name="mat"></param>
+        private static void PrintMat(bool[,] mat)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j <= mat.GetUpperBound(0); j++)
+            {
+                for (int k = 0; k <= mat.GetUpperBound(1); k++)
+                {
+                    sb.Append(mat[j, k] + ",");
+                }
+                sb.Append(Environment.NewLine);
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
--- C#/UnitTest/ProblemTests/Test10.cs
+++ C#/UnitTest/ProblemTests/Test10.cs
@@ -7,12 +7,14 @@ namespace UnitTest
     {
         [TestMethod]
         [DataRow("", "", true)]
-        [DataRow("*", "abbccc", true)]
+        [DataRow(".*", "abbccc", true)]
         [DataRow("a*", "abbccc", false)]
         [DataRow("a*", "aaa", true)]
         [DataRow("a.*", "abbccc", true)]
         [DataRow("ab.*", "abbccc", true)]
         [DataRow(".b.*", "abbccc", true)]
+        [DataRow("c*a*b", "aab", true)]
+        [DataRow("ab*a*c*a", "aaa", true)]
         public void TestMethod1(string pattern, string str, bool result)
         {
             Solution s = new();
