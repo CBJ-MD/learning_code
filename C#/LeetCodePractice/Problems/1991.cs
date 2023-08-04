e26317e06a89939e94207d248af9d3606ecf7e28
CBJ_MD <cbjmd15511661@gmail.com>
2023-08-04 12:59:05 +0800

    Added leet code no. 1991

---
+++ C#/LeetCodePractice/Problems/1991.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LeetCodePractice.Problems.Problem1991
+{
+    public class Solution
+    {
+        public int FindMiddleIndex(int[] nums)
+        {
+            int sum = 0;
+            int left = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > 0)
+                    left += nums[i - 1];
+                sum -= nums[i];
+                if (left == sum)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            return BinarySearch(nums, nums.Length, 0, target);
+        }
+
+        private int BinarySearch(int[] nums, int high, int low, int target)
+        {
+            int mid = (high + low) / 2;
+            if (mid == low)
+                return target > nums[mid] ? ++mid : mid;
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] > target)
+            {
+                return BinarySearch(nums, mid, low, target);
+            }
+            else
+            {
+                return BinarySearch(nums, high, mid, target);
+            }
+        }
+    }
+}
---
+++ C#/UnitTest/ProblemTests/Test1991.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodePractice.Problems.Problem1991;
+
+namespace UnitTest.ProblemTests
+{
+    [TestClass]
+    public class Test1991
+    {
+        [TestMethod]
+        [DataRow(new int[] { 2, 3, -1, 8, 4 }, 3)]
+        [DataRow(new int[] { 1, 7, 3, 6, 5, 6 }, 3)]
+        [DataRow(new int[] { 1, -1, 4 }, 2)]
+        [DataRow(new int[] { 2, 5 }, -1)]
+        public void TestMethod1(int[] nums, int result)
+        {
+            Solution s = new();
+            int calc = s.FindMiddleIndex(nums);
+            Assert.AreEqual(calc, result);
+        }
+    }
+}
